17e0745181102942e0b6a1078299b7752766398d
Alex Zabrodov <97849139+3as06@users.noreply.github.com>
2025-07-24 16:00:54 +0400

    Add initial Spring Boot project skeleton

---
+++ src/main/java/com/snackbery/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.snackbery.repository;
+
+import com.snackbery.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
